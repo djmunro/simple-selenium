1f9d8e53e3975819b37bc87b3bfd1dc8a84c2940
David Munro <david.munro@nokia.com>
2018-03-24 15:09:44 -0400

    adding initial blog

---
+++ src/layouts/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import g from "glamorous";
+import { css } from "glamor";
+import Link from "gatsby-link";
+
+import { rhythm } from "../utils/typography";
+
+const linkListStyle = css({ listStyle: `none`, float: `right` });
+
+const ListLink = props =>
+    <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+        <Link to={props.to}>
+            {props.children}
+        </Link>
+    </li>
+
+export default ({ children, data }) =>
+  <g.Div
+    margin={`0 auto`}
+    maxWidth={700}
+    padding={rhythm(2)}
+    paddingTop={rhythm(1.5)}
+  >
+    <header style={{ marginBottom: `1.5rem` }}>
+      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+      <g.H3 marginBottom={rhythm(2)} display={`inline`}>
+        {data.site.siteMetadata.title}
+      </g.H3>
+
+      </Link>
+      <ul className={linkListStyle}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/about/">About</ListLink>
+        <ListLink to="/contact/">Contact</ListLink>
+      </ul>
+    </header>
+    {children()}
+  </g.Div>
+
+export const query = graphql`
+  query LayoutQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
